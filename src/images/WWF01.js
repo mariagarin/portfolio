4b5534b279940a190447c94ca870d426a49506c7
Maria Garin <mariagarin@Marias-MacBook-Pro.local>
2021-06-15 16:35:36 -0700

    yesinstagramnoprtest

---
+++ src/images/WWF01.js
@@ -0,0 +1,19 @@
+// import Image from "../../src/components/Image";
+
+// const WWF01 = () => {
+  // return (
+    // <>
+      // <p>WWF01</p>
+      // <Image
+        // src="wwf01.png"
+        // className="mx-auto shadow-xl"
+        // alt="WWF01"
+        // style={{
+          // border: "10px solid green",
+        // }}
+      // />
+    // </>
+  // );
+// };
+
+// export default WWF01;
