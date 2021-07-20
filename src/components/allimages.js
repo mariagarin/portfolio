f7f2515daa3817b786e33e53c868825cc42b383b
Maria Garin <mariagarin@Marias-MacBook-Pro.local>
2021-07-19 17:06:16 -0700

    remove images for now

---
+++ src/components/allimages.js
@@ -0,0 +1,1078 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image";
+
+export function BLUE() {
+ return (
+   <StaticImage
+     src="../images/blue.png"
+     alt="BLUE"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function GMARED() {
+ return (
+   <StaticImage
+     src="../images/gmared.png"
+     alt="GMARED"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function GMASPIKES() {
+ return (
+   <StaticImage
+     src="../images/gmaspikes.png"
+     alt="GMASPIKES"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function GREENCOVER() {
+ return (
+   <StaticImage
+     src="../images/greencover.png"
+     alt="GREENCOVER"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function ZEST1() {
+ return (
+   <StaticImage
+     src="../images/zest1.png"
+     alt="ZEST1"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN01() {
+ return (
+   <StaticImage
+     src="../images/inn01.png"
+     alt="INN01"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN02() {
+ return (
+   <StaticImage
+     src="../images/inn02.png"
+     alt="INN02"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN03() {
+ return (
+   <StaticImage
+     src="../images/inn03.png"
+     alt="INN03"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN04() {
+ return (
+   <StaticImage
+     src="../images/inn04.png"
+     alt="INN04"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN05() {
+ return (
+   <StaticImage
+     src="../images/inn05.png"
+     alt="INN05"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN06() {
+ return (
+   <StaticImage
+     src="../images/inn06.png"
+     alt="INN06"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN07() {
+ return (
+   <StaticImage
+     src="../images/inn07.png"
+     alt="INN07"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+export function INN08() {
+ return (
+   <StaticImage
+     src="../images/inn08.png"
+     alt="INN08"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN09() {
+ return (
+   <StaticImage
+     src="../images/inn09.png"
+     alt="INN09"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN10() {
+ return (
+   <StaticImage
+     src="../images/inn10.png"
+     alt="INN10"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN11() {
+ return (
+   <StaticImage
+     src="../images/inn11.png"
+     alt="INN11"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN12() {
+ return (
+   <StaticImage
+     src="../images/inn12.png"
+     alt="INN12"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN13() {
+ return (
+   <StaticImage
+     src="../images/inn13.png"
+     alt="INN13"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN14() {
+ return (
+   <StaticImage
+     src="../images/inn14.png"
+     alt="INN14"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN15() {
+ return (
+   <StaticImage
+     src="../images/inn15.png"
+     alt="INN15"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN16() {
+ return (
+   <StaticImage
+     src="../images/inn16.png"
+     alt="INN16"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN17() {
+ return (
+   <StaticImage
+     src="../images/inn17.png"
+     alt="INN17"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN18() {
+ return (
+   <StaticImage
+     src="../images/inn18.png"
+     alt="INN18"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN19() {
+ return (
+   <StaticImage
+     src="../images/inn19.png"
+     alt="INN19"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN20() {
+ return (
+   <StaticImage
+     src="../images/inn20.png"
+     alt="INN20"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN21() {
+ return (
+   <StaticImage
+     src="../images/inn21.png"
+     alt="INN21"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN22() {
+ return (
+   <StaticImage
+     src="../images/inn22.png"
+     alt="INN22"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN23() {
+ return (
+   <StaticImage
+     src="../images/inn23.png"
+     alt="INN23"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN24() {
+ return (
+   <StaticImage
+     src="../images/inn24.png"
+     alt="INN24"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function INN25() {
+ return (
+   <StaticImage
+     src="../images/inn25.png"
+     alt="INN25"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+
+
+
+export function MMP01() {
+ return (
+   <StaticImage
+     src="../images/MMP01.png"
+     alt="MMP01"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP02() {
+ return (
+   <StaticImage
+     src="../images/MMP02.png"
+     alt="MMP02"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP03() {
+ return (
+   <StaticImage
+     src="../images/MMP03.png"
+     alt="MMP03"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP04() {
+ return (
+   <StaticImage
+     src="../images/mmp04.png"
+     alt="MMP04"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP05() {
+ return (
+   <StaticImage
+     src="../images/mmp05.png"
+     alt="MMP05"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP06() {
+ return (
+   <StaticImage
+     src="../images/mmp06.png"
+     alt="MMP06"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP07() {
+ return (
+   <StaticImage
+     src="../images/mmp07.png"
+     alt="MMP07"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+export function MMP08() {
+ return (
+   <StaticImage
+     src="../images/mmp08.png"
+     alt="MMP08"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP09() {
+ return (
+   <StaticImage
+     src="../images/mmp09.png"
+     alt="MMP09"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP10() {
+ return (
+   <StaticImage
+     src="../images/mmp10.png"
+     alt="MMP10"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP11() {
+ return (
+   <StaticImage
+     src="../images/mmp11.png"
+     alt="MMP11"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP12() {
+ return (
+   <StaticImage
+     src="../images/mmp12.png"
+     alt="MMP12"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP13() {
+ return (
+   <StaticImage
+     src="../images/mmp13.png"
+     alt="MMP13"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP14() {
+ return (
+   <StaticImage
+     src="../images/mmp14.png"
+     alt="MMP14"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP15() {
+ return (
+   <StaticImage
+     src="../images/mmp15.png"
+     alt="MMP15"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP16() {
+ return (
+   <StaticImage
+     src="../images/mmp16.png"
+     alt="MMP16"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP17() {
+ return (
+   <StaticImage
+     src="../images/mmp17.png"
+     alt="MMP17"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP18() {
+ return (
+   <StaticImage
+     src="../images/mmp18.png"
+     alt="MMP18"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP19() {
+ return (
+   <StaticImage
+     src="../images/mmp19.png"
+     alt="MMP19"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+export function MMP20() {
+ return (
+   <StaticImage
+     src="../images/mmp20.png"
+     alt="MMP20"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP21() {
+ return (
+   <StaticImage
+     src="../images/mmp21.png"
+     alt="MMP21"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP22() {
+ return (
+   <StaticImage
+     src="../images/mmp22.png"
+     alt="MMP22"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP23() {
+ return (
+   <StaticImage
+     src="../images/mmp23.png"
+     alt="MMP23"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP24() {
+ return (
+   <StaticImage
+     src="../images/mmp24.png"
+     alt="MMP24"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP25() {
+ return (
+   <StaticImage
+     src="../images/mmp25.png"
+     alt="MMP25"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP26() {
+ return (
+   <StaticImage
+     src="../images/mmp26.png"
+     alt="MMP26"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP27() {
+ return (
+   <StaticImage
+     src="../images/mmp27.png"
+     alt="MMP27"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP28() {
+ return (
+   <StaticImage
+     src="../images/mmp28.png"
+     alt="MMP28"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP29() {
+ return (
+   <StaticImage
+     src="../images/mmp29.png"
+     alt="MMP29"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP30() {
+ return (
+   <StaticImage
+     src="../images/mmp30.png"
+     alt="MMP30"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP31() {
+ return (
+   <StaticImage
+     src="../images/mmp31.png"
+     alt="MMP31"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP32() {
+ return (
+   <StaticImage
+     src="../images/mmp32.png"
+     alt="MMP32"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP33() {
+ return (
+   <StaticImage
+     src="../images/mmp33.png"
+     alt="MMP33"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP34() {
+ return (
+   <StaticImage
+     src="../images/mmp34.png"
+     alt="MMP34"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP35() {
+ return (
+   <StaticImage
+     src="../images/mmp35.png"
+     alt="MMP35"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP36() {
+ return (
+   <StaticImage
+     src="../images/mmp36.png"
+     alt="MMP36"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP37() {
+ return (
+   <StaticImage
+     src="../images/mmp37.png"
+     alt="MMP37"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP38() {
+ return (
+   <StaticImage
+     src="../images/mmp38.png"
+     alt="MMP38"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP39() {
+ return (
+   <StaticImage
+     src="../images/mmp39.png"
+     alt="MMP39"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP40() {
+ return (
+   <StaticImage
+     src="../images/mmp40.png"
+     alt="MMP40"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP41() {
+ return (
+   <StaticImage
+     src="../images/mmp41.png"
+     alt="MMP41"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP42() {
+ return (
+   <StaticImage
+     src="../images/mmp42.png"
+     alt="MMP42"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP43() {
+ return (
+   <StaticImage
+     src="../images/mmp43.png"
+     alt="MMP43"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP44() {
+ return (
+   <StaticImage
+     src="../images/mmp44.png"
+     alt="MMP44"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP45() {
+ return (
+   <StaticImage
+     src="../images/mmp45.png"
+     alt="MMP45"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function MMP46() {
+ return (
+   <StaticImage
+     src="../images/mmp46.png"
+     alt="MMP46"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF01() {
+ return (
+   <StaticImage
+     src="../images/wwf01.png"
+     alt="WWF01"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF02() {
+ return (
+   <StaticImage
+     src="../images/wwf02.png"
+     alt="WWF02"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF03() {
+ return (
+   <StaticImage
+     src="../images/wwf03.png"
+     alt="WWF03"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF04() {
+ return (
+   <StaticImage
+     src="../images/wwf04.png"
+     alt="WWF04"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF05() {
+ return (
+   <StaticImage
+     src="../images/wwf05.png"
+     alt="WWF05"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF06() {
+ return (
+   <StaticImage
+     src="../images/wwf06.png"
+     alt="WWF06"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF07() {
+ return (
+   <StaticImage
+     src="../images/wwf07.png"
+     alt="WWF07"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+export function WWF08() {
+ return (
+   <StaticImage
+     src="../images/wwf08.png"
+     alt="WWF08"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF09() {
+ return (
+   <StaticImage
+     src="../images/wwf09.png"
+     alt="WWF09"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF10() {
+ return (
+   <StaticImage
+     src="../images/wwf10.png"
+     alt="WWF10"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF11() {
+ return (
+   <StaticImage
+     src="../images/wwf11.png"
+     alt="WWF11"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF12() {
+ return (
+   <StaticImage
+     src="../images/wwf12.png"
+     alt="WWF12"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+export function WWF13() {
+ return (
+   <StaticImage
+     src="../images/wwf13.png"
+     alt="WWF13"
+     placeholder="blurred"
+     layout="fullWidth"
+   />
+ )
+}
+
+
+
+
+
+
+
+
+
+
